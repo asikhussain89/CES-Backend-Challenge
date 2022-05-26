f05df9e9f3997834a35d05f3fcb8563832a3908b
asikhussain89 <49228412+asikhussain89@users.noreply.github.com>
2022-05-26 11:21:10 +0530

    Delete src/config.js file

--- src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  GITHUB_PERSONAL_ACCESS_TOKEN: require('./token'),
-  GITHUB_OWNER_NAME: 'facebook',
-  GITHUB_REPO_NAME: 'react',
-}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const chalk = require('chalk')
-const config = require('./config')
+const commonConfig = require('./config/common-config')
+const contributorsCommentsListOfRepositoryFunc = require('./model/contributers-comments-list-of-repository')
 
-console.log(chalk.yellow('Your github token is:'))
-console.info(chalk.yellow(config.GITHUB_PERSONAL_ACCESS_TOKEN))
+// console.log(chalk.yellow('Your github token is:'))
+// console.info(chalk.yellow(commonConfig.GITHUB_PERSONAL_ACCESS_TOKEN))
 
-// remove this line
-require('./example')
+if (commonConfig.GITHUB_PERSONAL_ACCESS_TOKEN === '<TOKEN>' || commonConfig.GITHUB_PERSONAL_ACCESS_TOKEN === '') {
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log(chalk.red('Missing Token : Add the Github Personal Access Token in the config/common_config.js file'))
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+} else {
+    if (typeof process.argv[2] !== 'undefined' && typeof process.argv[3] !== 'undefined') {
+        if (process.argv[2] === "--repo") {
+            let inputNoOfDaysForFilterComments = 
+                (typeof process.argv[4] !== 'undefined' &&
+                    typeof process.argv[5] !== 'undefined') ? 
+                    process.argv[5].slice(0, process.argv[5].length - 1) : 0
+            contributorsCommentsListOfRepositoryFunc(process.argv[3], inputNoOfDaysForFilterComments)
+        } // Add Else if block for process any other parameters ( instead of --repo ) in future
+    } else {
+        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        console.log(chalk.red('Missing input : Run the app with proper inputs'))
+        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    }
+}
