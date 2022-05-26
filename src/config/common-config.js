c3d08dbddfe664985ff5bae4c6476236f2e3f015
asikhussain89 <49228412+asikhussain89@users.noreply.github.com>
2022-05-26 12:24:11 +0530

    Added common-config.js file

---
+++ src/config/common-config.js
@@ -0,0 +1,11 @@
+
+/**
+ * Export the GITHUB PERSONAL ACCESS TOKEN from this Module
+ * Add your GITHUB PERSONAL ACCESS TOKEN instead of <TOKEN> in below code
+ * Add any common configuration data of this app inside the below JSON {}
+ */
+ module.exports = {
+    GITHUB_PERSONAL_ACCESS_TOKEN: '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',
+    GITHUB_API_BASE_DOMAIN: 'https://api.github.com',
+  }
+  
