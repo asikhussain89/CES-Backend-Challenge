245532283fc0972a0ee2d153ab4757c58d541332
asikhussain89 <49228412+asikhussain89@users.noreply.github.com>
2022-05-26 12:21:05 +0530

    Added the model folder and its files

---
+++ src/model/contributers-comments-list-of-repository.js
@@ -0,0 +1,131 @@
+const leftPad = require('left-pad')
+const makeAsyncAPIConfig = require('../utils/make-single-or-multiple-async-APIs')
+
+/**
+ * 
+ * Export the below function to get the GitHub contributors Comments with Commit details
+ * Function receive first param as repository name
+ * Function receive second optional param as number of days which consider as started date from current date
+ * If second parameter not received, function will return all data
+ */
+module.exports = async function(repositoryName, noOfDaysForFilterComments) {
+    const contributorsCommentsListAPIUrlArray = [
+        `/repos/${repositoryName}/commits`,
+        `/repos/${repositoryName}/issues/comments`,
+        `/repos/${repositoryName}/pulls/comments`,
+    ]
+    const contributorsCommentsListAPIHttpMethodArray = ['GET', 'GET', 'GET']
+    let contributorsCommitsListAPIUrl = `/repos/${repositoryName}/stats/contributors`
+    let contributorsCommitsListAPIHttpMethod = 'GET'
+    contributorsCommentsListAPIUrlArray.push(contributorsCommitsListAPIUrl)
+    contributorsCommentsListAPIHttpMethodArray.push(contributorsCommitsListAPIHttpMethod)
+    const contributorsCommentsAndCommitListArray = 
+        await makeAsyncAPIConfig.makeMultipleAsyncAPIs(contributorsCommentsListAPIUrlArray, contributorsCommentsListAPIHttpMethodArray)
+    const contributorsCommentsAndCommitListObject = []
+    const contributorsIdListArray = []
+    let leftPadCount = 1
+    contributorsCommentsAndCommitListArray.forEach((data) => {
+        if (
+            typeof data.author !== 'undefined' &&
+            typeof data.author.id !== 'undefined' &&
+            typeof data.commit !== 'undefined' &&
+            typeof data.commit.author !== 'undefined' &&
+            typeof data.commit.author.name !== 'undefined' &&
+            typeof data.commit.author.date !== 'undefined'
+        ) {
+            if (
+                noOfDaysForFilterComments === 0 ||
+                new Date(data.commit.author.date).getTime() >
+                    new Date().setDate(new Date().getDate() - noOfDaysForFilterComments)
+            ) {
+                if (contributorsIdListArray.indexOf(data.author.id) !== -1) {
+                contributorsCommentsAndCommitListObject[contributorsIdListArray.indexOf(data.author.id)][
+                    data.author.id
+                ].commentCount += 1
+                if (
+                    contributorsCommentsAndCommitListObject[contributorsIdListArray.indexOf(data.author.id)][
+                        data.author.id
+                    ].commentCount.toString().length > leftPadCount
+                ) {
+                    leftPadCount += 1
+                  }
+                } else {
+                contributorsCommentsAndCommitListObject.push({
+                    [data.author.id]: {
+                        name: data.commit.author.name,
+                        commentCount: 1,
+                        commitCount: 0,
+                    },
+                })
+                contributorsIdListArray.push(data.author.id)
+                }
+            }
+        }
+
+        if (
+            typeof data.user !== 'undefined' &&
+            typeof data.user.id !== 'undefined' &&
+            typeof data.user.login !== 'undefined' &&
+            typeof data.created_at !== 'undefined'
+        ) {
+            if (
+                noOfDaysForFilterComments === 0 ||
+                new Date(data.created_at).getTime() >
+                    new Date().setDate(new Date().getDate() - noOfDaysForFilterComments)
+            ) {
+                if (contributorsIdListArray.indexOf(data.user.id) !== -1) {
+                contributorsCommentsAndCommitListObject[contributorsIdListArray.indexOf(data.user.id)][
+                    data.user.id
+                ].commentCount += 1
+                if (
+                    contributorsCommentsAndCommitListObject[contributorsIdListArray.indexOf(data.user.id)][
+                        data.user.id
+                    ].commentCount.toString().length > leftPadCount
+                ) {
+                    leftPadCount += 1
+                }
+                } else {
+                    contributorsCommentsAndCommitListObject.push({
+                        [data.user.id]: {
+                            name: data.user.login,
+                            commentCount: 1,
+                            commitCount: 0,
+                        },
+                    })
+                    contributorsIdListArray.push(data.user.id)
+                }
+            }
+        }
+  
+        if (
+            typeof data.total !== 'undefined' &&
+            typeof data.author.id !== 'undefined' &&
+            typeof data.author.login !== 'undefined'
+        ) {
+            if (contributorsIdListArray.indexOf(data.author.id) !== -1) {
+                contributorsCommentsAndCommitListObject[contributorsIdListArray.indexOf(data.author.id)][
+                    data.author.id
+                ].commitCount += 1
+                if (
+                    contributorsCommentsAndCommitListObject[contributorsIdListArray.indexOf(data.author.id)][
+                        data.author.id
+                    ].commitCount.toString().length > leftPadCount
+                ) {
+                    leftPadCount += 1
+                }
+            } else {
+                contributorsCommentsAndCommitListObject.push({
+                    [data.author.id]: {
+                        name: data.author.login,
+                        commentCount: 0,
+                        commitCount: 1,
+                    },
+                })
+                contributorsIdListArray.push(data.author.id)
+            }
+        }
+    })
+    contributorsIdListArray.forEach((value, index) => {
+        console.log(`${leftPad(contributorsCommentsAndCommitListObject[index][value].commentCount, leftPadCount,)} comments, ${contributorsCommentsAndCommitListObject[index][value].name} (${contributorsCommentsAndCommitListObject[index][value].commitCount} commits)`)
+    })
+}
